749edc33b120620d010083371bec1f8ccf4c87d5
Qu <hqu5@illinois.edu>
2016-11-17 20:33:40 -0600

    doug sucks

---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+    public float stepCount = 0;
+    public GameObject spooder;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+            spooder.GetComponent<Rigidbody>().velocity = new Vector3(Mathf.Sin(stepCount), 0, Mathf.Cos(stepCount));
+       
+
+        stepCount+= (float) .01;
+    }
+}
